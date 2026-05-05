816515c9c762db6a8265e60db56221cf3724af2c
muciger <german.mucino@gmail.com>
2026-05-05 12:04:12 -0600

    Agregar dashboard web (index.html y serve.py)

---
+++ serve.py
@@ -0,0 +1,9 @@
+import os, http.server, socketserver
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+PORT = 8080
+Handler = http.server.SimpleHTTPRequestHandler
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Servidor en http://localhost:{PORT}")
+    httpd.serve_forever()
